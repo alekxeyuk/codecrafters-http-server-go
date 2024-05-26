66a907aa31d57fd9132c980fb8fe395c3402a986
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2024-05-26 11:49:36 +0700

    status and header

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,10 +13,55 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
+}
+
+type httpStatus struct {
+	version string
+	code    int
+	reason  string
+}
+
+func (s *httpStatus) String() string {
+	return fmt.Sprintf("%s %d %s", s.version, s.code, s.reason)
+}
+
+type httpHeader []struct {
+	name  string
+	value string
+}
+
+func (h *httpHeader) String() string {
+	sb := new(strings.Builder)
+	for _, v := range *h {
+		sb.WriteString(fmt.Sprintf("%s: %s\r\n", v.name, v.value))
+	}
+	return sb.String()
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	status := httpStatus{
+		version: "HTTP/1.1",
+		code:    200,
+		reason:  "OK",
+	}
+	header := httpHeader{}
+
+	sb := strings.Builder{}
+	sb.WriteString(status.String())
+	sb.WriteString("\r\n")
+	sb.WriteString(header.String())
+	sb.WriteString("\r\n")
+
+	conn.Write([]byte(sb.String()))
 }
